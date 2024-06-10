8a5fd60d7cdd9d9e4835963cf6258677a1399aeb
BHUSHAN YADAV <132989870+beginner3a4@users.noreply.github.com>
2024-06-10 05:06:52 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=1223
+let email="bhushan@nsut.com"
+var password="12345"
+city="delhi"
+let state;
+
+// accountId=12 //not allowed
+
+console.log(accountId);
+console.table([accountId,email,password,city,state])
+
+/* prefer not to use var 
+  because it dont follow scope 
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as new version
+//alert(3+3)
+
+// we are using nodejs not browser
+
+
+/*
+datatypes
+number=> 2 to the power 53
+bigInt= store large no
+string=""
+boolean=true
+null= standalone value shows absence of value
+undefined=
+symbol= shows uniqueness
+*/
+
+console.log(typeof "bhushan");  //string
+console.log(typeof null);  //object
+console.log(typeof undefined); // undefined
