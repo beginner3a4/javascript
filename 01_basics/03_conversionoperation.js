0805d82448e71775f2bbdfd3126b7be04f84dbe4
BHUSHAN YADAV <132989870+beginner3a4@users.noreply.github.com>
2024-06-11 04:12:33 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,16 +1,16 @@
-let score="50abc"
-console.log(typeof score);
+// let score="50abc"
+// console.log(typeof score);
 
-let valueNumber=Number(score)
-console.log(valueNumber);
-// NaN not a number because we cant convert abc in number 
-console.log(typeof valueNumber);
-//number
+// let valueNumber=Number(score)
+// console.log(valueNumber);
+// // NaN not a number because we cant convert abc in number 
+// console.log(typeof valueNumber);
+// //number
 
-let temp=null
-let numtemp=Number(temp)
-console.log(numtemp);
-console.log(typeof numtemp);
+// let temp=null
+// let numtemp=Number(temp)
+// console.log(numtemp);
+// console.log(typeof numtemp);
 
 /*
 "33"=33
@@ -20,3 +20,12 @@ undefined=NaN
 true=1
 false=0
 */
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+console.log(+true);
+let num=100
+++num
+console.log(num);
